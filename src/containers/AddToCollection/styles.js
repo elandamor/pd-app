769d73eefa8b18b35d8371a478677f637dc3413d
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-09 15:17:53 +0200

    Created AddToCollection container

---
+++ src/containers/AddToCollection/styles.js
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #fafafa;
+  height: 100%;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 6;
+
+  header {
+    background-color: #ffffff;
+    border-bottom: thin solid #e8e8e8;
+    box-shadow: 0 1px 5px #e8e8e8;
+    height: 64px;
+    z-index: 2;
+  }
+
+  .c-btn--close {
+    height: 64px;
+    padding: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 64px;
+
+    .icon, svg {
+      height: 20px;
+      width: 20px;
+    }
+  }
+
+  section {
+    height: calc(100% - 120px);
+    overflow-y: auto;
+    padding: 8px;
+    z-index: 0;
+  }
+
+  footer {
+    background-color: #ffffff;
+    bottom: 0;
+    box-shadow: 0 -1px 5px #e8e8e8;
+    min-height: 48px;
+    position: absolute;
+    width: 100%;
+    z-index: 1;
+  }
+
+  /*
+   * Comments
+   */
+
+  .c-add-comment {
+    align-items: flex-start;
+    bottom: 0;
+    display: flex;
+    position: sticky;
+    min-height: 48px;
+
+    .c-avatar {
+      background-color: #adadad;
+    }
+
+    .c-textarea__wrapper {
+      flex: 1;
+      margin: 6px 0 4px;
+
+      textarea {
+        border: thin solid #ededed;
+        font-size: 14px;
+        margin: 0;
+        min-height: 32px;
+        padding: 6px 8px;
+      }
+    }
+
+    .c-actions {
+      align-self: flex-end;
+      height: 48px;
+    }
+
+    .c-btn {
+      &--toggle {
+        left: 0;
+        padding-left: 56px;
+        position: absolute;
+        justify-content: flex-start;
+        visibility: hidden;
+        width: 100%;
+      }
+
+      &--send {
+        background-color: #242424;
+        height: 32px;
+        margin: 8px 6px;
+        width: 32px;
+      }
+    }
+  }
+`;
+
+export default Wrapper;
