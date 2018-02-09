d405bdf983a9bd7dcfa0f20389f3a9944a55d9c7
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-09 15:17:27 +0200

    Created Textarea component

---
+++ src/components/Textarea/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import TextareaAutosize from 'react-autosize-textarea';
+
+const Wrapper = styled.div`
+  label {
+    color: ${(props) => props.theme.isDark ? '#ffffff' : '#000000'};
+    font-size: 13px;
+    font-weight: 500;
+    letter-spacing: 0.01rem;
+    margin: 0 12px;
+  }
+
+  textarea {
+    -webkit-appearance: none;
+    ${'' /* background-color: ${(props) => props.theme.palette.cardBackground}; */}
+    background-color: #fff;
+    color: ${(props) => props.theme.isDark ? '#ffffff' : '#000000'};
+    font-family: inherit;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0.01rem;
+    line-height: 1.5;
+    margin: 4px 0 2px;
+    outline: none;
+    padding: 8px 12px;
+    text-overflow: ellipsis;
+    width: 100%;
+  }
+`;
+
+const Textarea = (props) => {
+  const { id, label } = props;
+
+  return (
+    <Wrapper className="c-textarea__wrapper">
+      { label && <label htmlFor={id}>{label}</label> }
+      <TextareaAutosize {...props} />
+    </Wrapper>
+  );
+};
+
+Textarea.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
+};
+
+export default Textarea;
