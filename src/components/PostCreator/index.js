435ba29197e0ace78dee8075344eb13d634f9eca
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-12 18:18:13 +0200

    Minor refactor of PostCreator component

--- src/components/PostCreator/index.js
+++ src/components/PostCreator/index.js
@@ -2,20 +2,60 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // Components
 import Button from '../Button';
+import Dropdown from '../Dropdown';
 import Icon from '../Icon';
 import { ICONS } from '../Icon/constants';
 // Styled-Components
 import Wrapper from './styles';
 
+const tasks = [
+  {
+    name: 'Post',
+    value: 'post'
+  },
+  {
+    name: 'Product',
+    value: 'product'
+  },
+  {
+    name: 'Service',
+    value: 'service'
+  },
+];
+
 class PostCreator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTask: {},
+    };
+  }
+
+  handleSelector = (target) => {
+    this.setState({
+      currentTask: target,
+    });
+  }
+
   render() {
     const { className, onClose } = this.props;
+    const { currentTask } = this.state;
 
     return (
       <Wrapper
         className={`c-post-creator${className ? className : ''}`}
       >
         <header>
+          <Dropdown
+            id="c-task-selector"
+            name="tasks"
+            label="Add"
+            onChange={this.handleSelector}
+            options={tasks}
+            selected={tasks[0]}
+            srLabel="Choose a task"
+          />
           <Button
             className="c-btn--close"
             aria-label="Close"
@@ -24,7 +64,23 @@ class PostCreator extends Component {
             <Icon icon={ICONS.CLOSE} />
           </Button>
         </header>
-        <section></section>
+        <section>
+        {
+          currentTask.value === 'post' && (
+            <h1>Post</h1>
+          )
+        }
+        {
+          currentTask.value === 'product' && (
+            <h1>Product</h1>
+          )
+        }
+        {
+          currentTask.value === 'service' && (
+            <h1>Service</h1>
+          )
+        }
+        </section>
         <footer></footer>
       </Wrapper>
     );
