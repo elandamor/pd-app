1980b314b9f3071e57e5ea2704a1993c8ec05393
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-19 18:50:40 +0200

    Updated styles

--- src/components/Rating/styles.js
+++ src/components/Rating/styles.js
@@ -1,21 +1,28 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  .icon {
-    margin-right: 2px;
+  position: relative;
+`;
 
+const Star = styled.span`
+  display: inline-block;
+  margin-right: 2px;
+
+  svg {
+    fill: none;
+    stroke: #242424;
+  }
+
+  &.-active {
     svg {
-      fill: none;
-      stroke: #242424;
-    }
-
-    &.-active {
-      svg {
-        fill: orange;
-        stroke: orange;
-      }
+      fill: orange;
+      stroke: orange;
     }
   }
 `;
 
 export default Wrapper;
+
+export {
+  Star,
+};
