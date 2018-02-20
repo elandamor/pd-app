6bc973eae92c06dfbac7fc85df330fd711b4485e
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-20 20:50:37 +0200

    Refactor to self-contained render

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -5,24 +5,67 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as linkify from 'linkifyjs';
+import Linkify from 'linkifyjs/react';
+import hashtag from 'linkifyjs/plugins/hashtag';
+import mention from 'linkifyjs/plugins/mention';
+// Components
+import Image from '../../components/Image';
 // Styles
 import Wrapper from './styles';
+
+hashtag(linkify);
+mention(linkify);
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Message extends Component {
   render() {
-    const { children, ...rest } = this.props;
+    const { attachment, body, direction, ...rest } = this.props;
 
     return (
       <Wrapper {...rest}>
-        {children}
+        <div className={`message ${direction}`}>
+          {
+            attachment && attachment.exists
+            && attachment.type === 'image' && (
+              <Image
+                src={attachment.source}
+                alt={body}
+              />
+            )
+          }
+          <Linkify
+            options={{
+              attributes: {
+                onClick: (evt) => evt.preventDefault(),
+                rel: 'nofollow me noopener noreferrer',
+              },
+              className: {
+                hashtag: 'c-hashtag',
+                url: 'c-url',
+              },
+              formatHref: {
+                hashtag: (val) => `/search?q=${val.substr(1)}&src=hashtag_click`,
+              },
+              nl2br: true,
+            }}
+          >
+            {body}
+          </Linkify>
+        </div>
       </Wrapper>
     );
   }
 }
 
+Message.defaultProps = {
+  attachment: {},
+};
+
 Message.propTypes = {
-  children: PropTypes.node.isRequired,
+  attachment: PropTypes.object,
+  body: PropTypes.object.isRequired,
+  direction: PropTypes.object.isRequired,
 };
 
 export default Message;
